61f81bedb8f7cc6fbca5206e5c5e8f3fcfd64353
Spencer Ward <spencertward@gmail.com>
2017-04-20 04:42:34 -0600

    Removed App.css

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,7 +6,7 @@ const Home = (props) => {
     <div>
       <h2>You live on {props.homePlanet}</h2>
       <Link to='/solarsystem'>
-        Let's look at the planets of the solar system.
+        Look at the planets of the solar system.
         <img src="/images/home.gif" />
       </Link>
       <a href="/solarsystem">Solar System - just a regular link (not React - this link will cause a page load)</a>
